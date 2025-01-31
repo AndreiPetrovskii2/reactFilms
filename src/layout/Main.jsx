33e7efa543844d94136b379c092cb17cffd6992d
Андрей Петровский <andrei_2001pet@mail.ru>
2025-01-31 19:02:15 +0500

    https fetch

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,7 +13,7 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix&type=`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix&type=`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search , loading:false }));
   }
@@ -21,7 +21,7 @@ class Main extends React.Component {
   searchMovies = (str, type = "all") => {
     this.setState({loading : true});
     fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
